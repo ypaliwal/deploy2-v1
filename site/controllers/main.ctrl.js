2012a4ba8d138e59b43c12e6f757afa01934ec5e
ypaliwal <y.paliwal92@gmail.com>
2016-05-09 17:39:03 -0400

    next commit

--- site/controllers/main.ctrl.js
+++ site/controllers/main.ctrl.js
@@ -4,22 +4,19 @@
 		.module('TPapp')
 		.controller('MainCtrl', MainCtrl);
 
-	function MainCtrl($state,$location) {
+	function MainCtrl($state,$location, projectSrv) {
 		var mainVm = this;
 		console.log('Main page loaded correctly.');
 
-
+		// Public functions/data
 		mainVm.toMap = toMap;
+		mainVm.projectSrv = projectSrv;
 
 		function toMap() {
 			console.log('This works');
 			$location.path('/map');
-			// $state.go('map');
 		}
 
-		
-
 	}
-
 
 })();
